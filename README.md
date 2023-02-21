13d88792335f02ea1c4b9df5612f07d02e3c2027
Tarek Laun <tareklaun@hotmail.com>
2023-02-21 12:41:53 +0100

    Added Cloud flare Support

--- README.md
+++ README.md
@@ -4,11 +4,20 @@ A NodeJS DynDNS Client.
 ## Config 
 ```json
 {
-    "DNSs": [{
+    "Records": [{
         "Service": "Strato",
         "Hostname": "domain.net",
         "Domain": "subdomain.domain.net",
         "Password": "Password"
+    },
+    {
+        "Service": "CloudFlare",
+        "Email": "Cloud Flare Email.",
+        "ApiKey": "Cloud Flare Api Key",
+        "Domain": "Domain Name",
+        "Zone": "Zone ID",
+        "Record": "Record Id",
+        "Proxied": true
     }],
     "IpCheckInterval": 1 
 }
--- main.js
+++ main.js
@@ -11,18 +11,20 @@ console.log("-       © Tarek Laun       -");
 console.log("----------------------------");
 
 function UpdateAllDomains() {
-    var dns = config["DNSs"];
+    var dns = config["Records"];
     dns.forEach(element => {
-        UpdateDomain(element["Hostname"], element["Password"], element["Domain"], ip, element["Service"]);
+        UpdateDomain(element["Hostname"], element["Password"], element["Domain"], ip, element["Service"], element["Email"], element["ApiKey"], element["Zone"], element["Record"], element["Proxied"]);
     })
 }
 
-function UpdateDomain(hostname, password, domain, _ip, service) {
+function UpdateDomain(hostname, password, domain, _ip, service, mail, apiKey, zone, record, pro) {
     console.log("[DynDNS] Update Domain " + domain + " to ip " + _ip + ".");
     if (service == "Strato") {
         UpdateStratoDomain(hostname, password, domain, _ip)
+    }else if (service == "CloudFlare") {
+        UpdateCloudFlareDomain(zone, record, pro,_ip, domain, mail, apiKey);
     }else {
-        console.log("[DnyDNS] Cannot finde Service.");
+        console.log("[Error] Cannot find Service.")
     }
 }
 
@@ -35,6 +37,19 @@ function UpdateStratoDomain(hostname, password, domain, _ip) {
         .catch(error => {
         console.error(error);
     });
+}
+
+function UpdateCloudFlareDomain(zone, record, proxied, ip, domain, mail, apiKey) {
+    axios.patch('https://api.cloudflare.com/client/v4/zones/' + zone + '/dns_records/' + record,
+    { "content": ip, "name": domain, "proxied": proxied, "ttl": 86400 },
+    { headers: { 'Content-Type': 'application/json', 'X-Auth-Email': mail, "X-Auth-Key": apiKey }, }
+    ).then((response) => {
+        // Code
+        console.log("[DynDNS] Update Successful.");
+    }).catch((error) => {
+        // Code
+        console.error(error);
+    })
 }
 
 function UpdateIp() {
